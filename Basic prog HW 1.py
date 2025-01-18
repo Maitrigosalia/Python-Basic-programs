2508c3b8c807731dd46ed706f8657bbb37953798
Maitri Gosalia <maitri.gosalia25@gmail.com>
2025-01-18 23:46:51 +0530

    Add files via upload
    This file includes basic program questions for beginners to gain hands-on proficiency in Python.

---
+++ Basic prog HW 1.py
@@ -0,0 +1,196 @@
+def Q1():
+    #Print the text
+    print ("Welcome to C-Tag")
+#Q1()
+
+def Q2():
+    #Print an Integer
+    Integer= eval(input("Enter Integer"))
+    print(Integer)
+#Q2()
+
+
+def Q3():
+    #Print mail address in the format
+    print("-------------------------------------------")
+    print("Mailing address\t: gosaliamaitri@gmail.com")
+    print("-------------------------------------------")
+#Q3()
+
+def Q4():
+    #Add 2 numbers
+    a= eval(input("Enter No 1"))
+    b= eval(input("Enter No 2"))
+    c= a + b
+    print ("Addition of two Integers :", c)
+#Q4()
+
+def Q5():
+    #Multiply two numbers
+    a= eval(input("Enter No 1 :"))
+    b= eval(input("Enter No 2 : "))
+    c= a * b
+    print ("Multiplication of Two Integers :", c)
+#Q5()
+
+def Q6():
+    #Multiplication Table
+    a= eval(input("Number 1 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 2 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 3 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 4 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 5 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 6 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 7 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 8 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 9 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+
+    a= eval(input("Number 10 :"))
+    b= 8 * a
+    print ("Multiplication Table of 8 :", b)
+#Q6()
+
+def Q7():
+    #Addition and Subtraction of Two Numbers
+    a= eval(input("Number 1 :"))
+    b= eval(input("Number 2 :"))
+    c= a + b
+    print("Addition of Two Numbers :",c)
+
+    c= a - b
+    print("Substraction of Two Numbers:",c)
+#Q7()
+
+def Q8():
+    #Sum of two integers
+    a= eval(input("Number 1 :"))
+    b= eval(input("Number 2 :"))
+    c= a + b
+    print("Addition of Two Numbers :",c)   
+#Q8()
+
+def Q9():
+    #Addition and Multiplication of two numbers
+    a= eval(input("Number 1 :"))
+    b= eval(input("Number 2 :"))
+    c= a + b
+    print("Addition of Two Numbers :",c)
+
+    c= a * b
+    print("Multiplication of Two Numbers :",c)
+#Q9()   
+
+def Q10():
+    #Division of two integers
+    a= eval(input("Number 1 :"))
+    b= eval(input("Number 2 :"))
+    c= a + b
+    print("Addition of Two Numbers :",c)
+
+    c= a / b
+    print("Division of Two Numbers :",c)
+#Q10()
+   
+def Q11():
+    #Compute Quotient of number
+    a= eval(input("Number 1 :"))
+    b= eval(input("Number 2 :"))
+    c= a / b
+    print ("Quotient of Two Numbers", c)
+#Q11()
+
+def Q12():
+    #Remainder of number
+    a= eval(input("Number 1 :"))
+    b= eval(input("Number 2 :"))
+    c= a % b
+    print ("Remainder of Two Numbers", c)
+#Q12()
+
+def Q13():
+    #Convert Temperature between Celsius and Fahrenhiet
+    a= eval(input("Temperature between Celsius and Fahrenhiet:"))
+    print ("Temperature in Celsius :",a)
+
+    b= (a - 32) * 5 / 9
+    print ("Temperature in Fahrenhiet :",b)
+#Q13()
+    
+def Q14():
+    #Convert Degree Celsius to Fahrenhiet
+    a= eval(input("Degree Celsius to Fahrenhiet"))
+    b= (a * 9/5) + 32
+    print("Temperature Celsius to Fahrenhiet", b)
+#Q14()
+
+def Q15():
+    #Convert Degree Fahrenhiet to Celsius
+    a= eval(input("Fahrenhiet to Celsius :"))
+    b= (a - 32) * 5/9
+    print("Fahrenhiet to Celsius", b)
+#Q15()
+
+def Q16():
+    #Convert Centimeter to Meter
+    a= eval(input("Centimeter to Meter :"))
+    ans= a / 100
+    print("Centimeter to Meter", ans)
+
+    a= eval(input("Centimeter to km :"))
+    ans= a / 1000
+    print ("Centimeter to km", ans)
+#Q16()
+
+import math
+def Q17():
+    #Area of circle
+    r= eval(input("Enter Radius :"))
+    ans= math.pi * r * r
+    print("Area of Circle", ans)
+#Q17()
+
+def Q18():
+    #Area of rectangle
+    length= eval(input("Enter length :"))
+    breath= eval(input("Enter breath :"))
+    ans= length * breath
+    print("Area of rectangle", ans)
+#Q18()
+
+def Q19():
+    #Area of Triangle
+    length= eval(input("Enter length :"))
+    height= eval(input("Enter height :"))
+    ans= 0.5 * length * height
+    print("Area of Triangle", ans)
+#Q19()
+
+
+
+
